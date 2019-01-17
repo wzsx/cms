877fffabd28a391bb188c7e838cf3d90587d7b56
wzsx <1217742369@qq.com>
2019-01-17 10:15:43 +0800

    1.7

---
+++ app/Admin/Controllers/GoodsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+class GoodsController extends Controller
+{
+}
