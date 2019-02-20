e6b43d8da43f21ae82e84d0bdf4f09d224192553
wzsx <1217742369@qq.com>
2019-02-20 10:40:30 +0800

    wwww

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -57,7 +57,7 @@ class WeixinController extends Controller
                 }
             } elseif ($xml->MsgType == 'voice') {        //处理语音信息
                 $this->dlVoice($xml->MediaId);
-                $xml_response = '<xml><ToUserName><![CDATA[' . $openid . ']]></ToUserName><FromUserName><![CDATA[' . $xml->ToUserName . ']]></FromUserName><CreateTime>' . time() . '</CreateTime><MsgType>< ![CDATA[voice]]></MsgType><Voice><MediaId><![CDATA[media_id] ]></MediaId></Voice></xml>';
+                $xml_response = '<xml><ToUserName><![CDATA[' . $openid . ']]></ToUserName><FromUserName><![CDATA[' . $xml->ToUserName . ']]></FromUserName><CreateTime>' . time() . '</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[' . str_random(10) . ' >>> ' . date('Y-m-d H:i:s') . ']]></Content></xml>';
                 echo $xml_response;
             } elseif ($xml->MsgType == 'event') {        //判断事件类型
                 //exit();
