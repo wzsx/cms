661bb7e8bd103280fa718ceaf09a25d25f3ceff0
wzsx <1217742369@qq.com>
2019-02-23 08:43:54 +0800

    1111

--- resources/views/weixin/weixin.blade.php
+++ resources/views/weixin/weixin.blade.php
@@ -4,10 +4,10 @@
 @section('content')
     <form  action="/form/test" method="post" enctype="multipart/form-data" >
         <input type="text" name="fs">
-        <h1>上传</h1>
+        {{--<h1>上传</h1>--}}
         {{csrf_field()}}
-        <input type="file" name="media">
-        <button type="submit" class="btn btn-default">上传</button>
+        {{--<input type="file" name="media">--}}
+        <button type="submit" class="btn btn-default">发送</button>
     </form>
 @endsection
 
