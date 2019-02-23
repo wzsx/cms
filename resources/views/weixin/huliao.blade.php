8c3a6dcd401bbb3f21a7aed08faef03d9d012bb5
wzsx <1217742369@qq.com>
2019-02-23 09:06:41 +0800

    23424224

--- resources/views/weixin/huliao.blade.php
+++ resources/views/weixin/huliao.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <form  action="/form/hl" method="post" enctype="multipart/form-data" >
-       <input type="textarea" name="fs">
+        <textarea name="fs" id="" cols="30" rows="10"></textarea>
         {{csrf_field()}}
         <button type="submit" class="btn btn-default">发送</button>
     </form>
