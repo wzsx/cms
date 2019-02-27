8a09e80954792b421ed319cf809ba61d7f5bc124
wzsx <1217742369@qq.com>
2019-02-27 14:53:25 +0800

    aaaa

--- resources/views/orders/list.blade.php
+++ resources/views/orders/list.blade.php
@@ -8,7 +8,7 @@
                 <li>订单ID: {{$v['order_sn']}} --  订单总价：¥{{$v['order_amount'] / 100}}   --  下单时间：{{date('Y-m-d H:i:s',$v['add_time'])}}
                     <a href="/pay/o/{{$v['oid']}}" class="btn btn-info">支付宝支付</a><br><br>
                     <a href="/weixin/o/{{$v['oid']}}" class="btn btn-info">微信支付</a><br><br>
-                    <a href="/order/del/{{$v['oid']}}" class="del_goods">删除</a>
+                    {{--<a href="/order/del/{{$v['oid']}}" class="del_goods">删除</a>--}}
                 </li>
             @endforeach
         </ul>
