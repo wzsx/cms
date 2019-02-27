1d6bc6a99bd8857a35827b7b73af3705868b9899
wzsx <1217742369@qq.com>
2019-02-27 10:33:26 +0800

    wx

--- resources/views/weixin/erma.blade.php
+++ resources/views/weixin/erma.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <div id="qrcode"></div>
-    <script type="text/javascript" src="/js/qrcode/qrcode.js"></script>
+    <script type="text/javascript" src="/js/qrcode/qrcode.min.js"></script>
 </head>
 <body>
 
