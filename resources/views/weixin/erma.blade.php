93dcfb1604cf195f3fa035b1c4641afd60d02216
wzsx <1217742369@qq.com>
2019-02-27 10:36:46 +0800

    wx

--- resources/views/weixin/erma.blade.php
+++ resources/views/weixin/erma.blade.php
@@ -5,21 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <div id="qrcode"></div>
-    <script type="text/javascript" src="/js/qrcode/qrcode.min.js"></script>
+    <title>支付页面</title>
 </head>
 <body>
-
+<div id="qrcode" align="center"></div>
+<h2 align="center">扫&nbsp;&nbsp;码&nbsp;&nbsp;付&nbsp;&nbsp;款</h2>
 </body>
 </html>
-<script>
-    var qrcode = new QRCode("test", {
+<script src="{{ URL::asset('js/qrcode/qrcode.min.js') }}"></script>
+<script type="text/javascript">
+    var qrcode = new QRCode(document.getElementById('qrcode'), {
         text: "{{$url}}",
         width: 256,
         height: 256,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
+        colorDark: '#000000',
+        colorLight: '#ffffff',
+        correctLevel: QRCode.CorrectLevel.H
     });
 </script>
+
