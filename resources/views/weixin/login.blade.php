fb44b9a7171bf7a62dd702f831122e2caa4e503d
wzsx <1217742369@qq.com>
2019-02-28 14:25:36 +0800

    qqqq

--- resources/views/weixin/login.blade.php
+++ resources/views/weixin/login.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h2>微信登录</h2>
         <h3>
-            <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dhttp%3a%2f%2fxiuge.52self.cn%2fweixin%2fgetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">Login</a>
+            <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dhttp%3a%2f%2fxiuge.52self.cn%2fweixin%2fgetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">登录</a>
         </h3>
     </div>
 @endsection
