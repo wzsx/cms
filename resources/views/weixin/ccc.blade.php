c0b04710c162c664e8ea5947fd514e719e0b7cd2
wzsx <1217742369@qq.com>
2019-03-08 11:48:22 +0800

    ppp

--- resources/views/weixin/ccc.blade.php
+++ resources/views/weixin/ccc.blade.php
@@ -3,22 +3,23 @@
     <tr>
         <td><button type="submit" class="btn btn-default">一级按钮</button>名字:<input type="text" name="name"> <input type="button" value="克隆" style="width: 50px;" class="btn"></td>
     </tr>
-    <tr><td><button type="submit" class="btn btn-default">二级按钮</button>   <input type="button" value="克隆" style="width: 50px;" class="btn1"></td></tr>
-    <tr><td>按钮类型:     <select name="" id="cd">
-            <option value="">请选择</option>
-            <option value="">一级按钮</option>
-            <option value="">二级按钮</option>
-        </select></td>
-    </tr>
-    <tr>
-        <td>二级按钮名字:<input type="text"></td>
-    </tr>
-    <tr>
-        <td>二级按钮url:<input type="text"></td>
-    </tr>
-    <tr>
-        <td>二级按钮名字key:<input type="text"></td>
-    </tr>
+    <tr><td><button type="submit" class="btn btn-default">二级按钮</button> 按钮类型:     <select name="" id="cd">
+                <option value="">请选择</option>
+                <option value="">一级按钮</option>
+                <option value="">二级按钮</option>
+            </select>
+        二级按钮名字:<input type="text">二级按钮url:<input type="text">二级按钮名字key:<input type="text"><input type="button" value="克隆" style="width: 50px;" class="btn1"></td></tr>
+    {{--<tr>--}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+        {{----}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+        {{----}}
+    {{--</tr>--}}
+    {{--<tr>--}}
+        {{----}}
+    {{--</tr>--}}
 </table>
 
 <script>
@@ -27,8 +28,8 @@
             var _this=$(this);
             var _val=_this.val();
             if(_val=='克隆'){
-                var _tr=_this.parents('tr').clone();
-                _this.parents('tr').after(_tr);
+                var _tr=_this.parents('table').clone();
+                _this.parents('table').after(_tr);
             }
         })
         $(document).on('click','.btn1',function(){
