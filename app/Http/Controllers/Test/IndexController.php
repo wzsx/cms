eb57fd0eb900fd738c2786b22881cbd71f6f93e6
wzsx <1217742369@qq.com>
2019-03-22 16:37:16 +0800

    111

--- app/Http/Controllers/Test/IndexController.php
+++ app/Http/Controllers/Test/IndexController.php
@@ -15,32 +15,7 @@ class IndexController extends Controller{
         return view('test.index',$data);
     
     }
-    public function pss(){
-        $pass=$_POST['pass'];
-        $email=$_POST['email'];
-        $id = UserModel::where(['email'=>$email])->first();
-        if(password_verify($pass,$id->pass)){
-            $token = substr(md5(time().mt_rand(1,99999)),10,10);
-            setcookie('token',$token,time()+86400,'/','xiuge.52self.cn',false,true);
-            setcookie('uid',$id->uid,time()+86400,'/','xiuge.52self.cn',false,true);
-            $redis_key_web='str:u:pass:'.$id->uid;
-            Redis::set($redis_key_web,$token);
-            Redis::expire($redis_key_web,86400);
-            $response=[
-                'errno'=>0,
-                'msg'=>'登录成功'
-            ];
-        } else {
-            $response=[
-                'errno'=>5001,
-                'msg'=>'用户密码不正确'
-            ];
-
-        }
-            return $response;
-    }
-
-
+    
     public function apiLogin(Request $request){
         $email=$request->input('email');
         $password=$request->input('pass');
