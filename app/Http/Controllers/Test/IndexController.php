b6b512ba5fca7f09bc06c91e350a5f0e557976b9
wzsx <1217742369@qq.com>
2019-03-22 18:35:19 +0800

    111

--- app/Http/Controllers/Test/IndexController.php
+++ app/Http/Controllers/Test/IndexController.php
@@ -64,7 +64,7 @@ class IndexController extends Controller{
          //执行命令
          $r2 = curl_exec($curl);
          $re2=json_decode($r2,true);
-        var_dump($re2);die;
+        //var_dump($re2);die;
          return $re2;
         
        }
