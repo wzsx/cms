588ae91544234522095eca42d2c9b68f6bcb662d
wzsx <1217742369@qq.com>
2019-03-22 11:01:34 +0800

    111

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -18,6 +18,7 @@ class VerifyCsrfToken extends Middleware
         '/weixin/valid',
         '/weixin/valid1',
         '/weixin/material',
-        '/weixin/pay/notice'
+        '/weixin/pay/notice',
+        '/pss'
     ];
 }
