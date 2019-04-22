bf493cd247d2aea621eb1ed0787a17a394839883
wzsx <1217742369@qq.com>
2019-04-22 11:13:09 +0800

    dsdsdssds

--- app/Http/Controllers/Xm/XmController.php
+++ app/Http/Controllers/Xm/XmController.php
@@ -13,34 +13,38 @@ class XmController extends Controller{
     }
     public function demo2(Request $request)
 
-    {
+{
 
 
-        $name = $request->input('name');
-        $password = $request->input('pwd');
+    $name = $request->input('name');
+    $password = $request->input('pwd');
 
-        //echo $password;
+    //echo $password;
 
         $uid = $name;
 
         $key = 'token:' . $uid;
-        $token = substr(md5(time() + $uid + rand(1000,9999)),10,20);
-        Redis::del($key);
-        Redis::set($key,$token);
-        header("refresh:0;/demo3?token=$token&uid=$uid");
-    }
-    public function demo3(Request $request){
-        $token=$request->input('token');
-        $uid=$request->input('uid');
 
-        $key = 'token:' . $uid;
+        $token = Redis::get($key);
 
-        $tokens = Redis::get($key);
-        if($token!=$tokens){
-            echo 'NO';
-        }else{
-            echo "OK";
+    if(!empty($token)){
+            $token = substr(md5(time() + $uid + rand(1000,9999)),10,20);
+            Redis::del($key);
+            Redis::set($key,$token);
+
         }
+    header("refresh:0;/demo3?token=$token");
+}
+public function demo3(Request $request){
+    $token=$request->input('token');
+    $key = 'token:' . 123;
+
+    $tokens = Redis::get($key);
+    if($token!=$tokens){
+        echo 'NO';
+    }else{
+        echo "OK";
     }
+}
 }
 ?>
